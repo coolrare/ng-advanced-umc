cdd6535afbad4b31e21d71ec63f6c38abc90b9fe
Will <doggy.huang@gmail.com>
2021-11-19 16:13:22 +0800

    完成 LoginComponent

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+    document.body.className = 'bg-gradient-primary';
+  }
+
+  ngOnDestroy(): void {
+    document.body.className = '';
   }
 
 }
