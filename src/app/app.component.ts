ad8fde34fdb2f6cd7f5332a14d3bb3f875158cb4
Will <doggy.huang@gmail.com>
2021-11-19 11:30:33 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { chartAreaDemo } from './chartAreaDemo';
+import { chartPieDemo } from './chartPieDemo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-advanced-umc';
+export class AppComponent implements OnInit {
+  title = 'demo1';
+
+  ngOnInit(): void {
+  }
 }
