352c3de24d5737f3b19a699d6e9f31bea2ae9ae6
Will <doggy.huang@gmail.com>
2021-11-26 11:12:26 +0800

    加入表單驗證的 UI 邏輯判斷

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,8 +7,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit, OnDestroy {
 
   data: any = {
-    email: 'user01@example.com',
-    password: '123',
+    email: '',
+    password: '',
     isRememberMe: true
   };
 
