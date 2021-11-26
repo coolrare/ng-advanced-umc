336e6cc01c6b13cb68f3418d7632da05c78c4506
Will <doggy.huang@gmail.com>
2021-11-26 16:01:18 +0800

    設定 ValidatePasswordComplexity 不用檢查沒有輸入密碼的情況

--- src/app/shared/ValidatePasswordComplexity.ts
+++ src/app/shared/ValidatePasswordComplexity.ts
@@ -1,6 +1,9 @@
 import { FormControl } from '@angular/forms';
 
 export function ValidatePasswordComplexity(control: FormControl) {
+  if (!control.value) {
+    return null;
+  }
   var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,30}$/;
   return re.test(control.value) ? null : { PasswordComplexity: true };
 }
